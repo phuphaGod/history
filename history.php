eab4a94b3057bb519174505dc6817cf20a69ba4d
phuphaGod <phuphaphupha444@gmail.com>
2024-12-16 13:03:47 +0700

    1

---
+++ history.php
@@ -0,0 +1,21 @@
+<html lang="th">
+<head>
+    <title>แนะนำตัวเอง</title>
+    <link rel="stylesheet" href="historo.css">  
+    <meta charset="UTF-8">
+</head>
+<body>
+<font size="5">
+<?php
+    $x = 55;
+    $y = 20;
+    $z = 170;
+    $a = 6606021621143;
+    $b = 2;
+    echo "<h1> ผมนายธนภูมิ รักศีล <h1> อายุ " . $y . " ปี<br/>";
+    echo "<h1>ส่วนสูง " .$z. " น้ำหนัก " .$x. " รหัสนักศึกษา " .$a. "<br/>";
+    echo "<h1> ศีกษาอยู่ที่มหาลัยพระจอมเกล้าพระนครเหนือวิทยาเขตปราจีน  sec" .$b. "<br/>";
+?>
+</font>
+</body>
+</html>
